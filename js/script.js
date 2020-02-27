e09bfc306c4bddb0881a7fdf83b5a9d090e34f5e
amilord <60766420+amilord@users.noreply.github.com>
2020-02-26 22:56:10 -0500

    random quote generator

--- js/script.js
+++ js/script.js
@@ -11,23 +11,76 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+var quotes = [
+  { quote: 'Everything I did wrongly is an experience… to be honest and truthful in all endeavours is an experience, not a regret.',
+          source: 'Fela Kuti',
+                 citation: 'https://thisisafrica.me/lifestyle/felabration-week-remembering-fela-kuti-10-quotes/',
+  },
+
+  { 
+    quote: 'Nobody in the world, nobody in history, has ever gotten their freedom by appealing to the moral sense of the people who were oppressing them.',
+          source: 'Assata Shakur',
+                 citation: 'Assata: An Autobiography',
+                          year: '1987'
+  },
+
+  {
+    quote: 'The greatest weapon in the hand of the oppressor is the mind of the oppressed.',
+          source: 'Steven Biko',
+  },
+
+  {
+    quote: 'Death is not the greatest loss in life. The greatest loss is what dies inside while still alive. Never surrender.',
+          source: 'Tupac Shakur',
+  },
+
+  {
+    quote: 'Without a game plan and without a strong sense of faith in what you’re doing, it’s gonna be real hard to accomplish anything.',
+          source: 'Nipsey Hussle',
+  
+  }
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote(array) {
+  var quoteIndex = Math.floor( Math.random() * (quotes.length));
+            for (var i = 0; i < array.length; i++) {
+                    var randomQuote = array[quoteIndex];
+}
+return randomQuote;
+}
+var randomQuote = getRandomQuote(quotes);
+console.log(randomQuote);
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  var yourStringHere = "";    
+  var randomQuote = getRandomQuote(quotes);
+                 yourStringHere += "<p class='quote'>" + randomQuote.quote + "</p>";
+                                 yourStringHere += "<p class='source'>" + randomQuote.source;
+                                                 yourStringHere += "<span class='citation'>" + randomQuote.citation + "</span>";
+                                                                yourStringHere += "<span class='year'>" + randomQuote.year + "</span>"
+                                                                                yourStringHere += "</p>";
 
+  document.getElementById('quote-box').innerHTML = yourStringHere;
+}
 
+printQuote();
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+Steps taken to complete this project. 
+I downloaded the zips files which consisted of css, html, and js and opened them up in visual studio text editor. Since the HTML and CSS file was already writtedn out I proceeded to work on the js. I at first wrote my array with multiple strings, but realized thats not what the instructipns called for so i created objects within an array with a var = quote. The 2nd thing I procceeded to do was add quotes and sources to my opbjects. Then the function getrandomquote which creates a random number was written out. This part of the project is not fully my work I googled and found an example function that I used from treehouse
+https://teamtreehouse.com/community/i-need-help-with-the-random-quote-generator-project. 
+I then created a function that shows a new quote everytime the button is clicked and below that 
