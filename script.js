164b655574f08e778bd2dc57dc7abafc2693cbcd
amilord <60766420+amilord@users.noreply.github.com>
2020-03-05 19:28:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -15,28 +15,32 @@ var quotes = [
   { quote: 'Everything I did wrongly is an experience… to be honest and truthful in all endeavours is an experience, not a regret.',
           source: 'Fela Kuti',
                  citation: 'https://thisisafrica.me/lifestyle/felabration-week-remembering-fela-kuti-10-quotes/',
+                          year:
   },
 
   { 
     quote: 'Nobody in the world, nobody in history, has ever gotten their freedom by appealing to the moral sense of the people who were oppressing them.',
           source: 'Assata Shakur',
                  citation: 'Assata: An Autobiography',
-                          year: '1987'
+                          year: 1987
   },
 
   {
     quote: 'The greatest weapon in the hand of the oppressor is the mind of the oppressed.',
           source: 'Steven Biko',
+                 
   },
 
   {
     quote: 'Death is not the greatest loss in life. The greatest loss is what dies inside while still alive. Never surrender.',
           source: 'Tupac Shakur',
+                 
   },
 
   {
     quote: 'Without a game plan and without a strong sense of faith in what you’re doing, it’s gonna be real hard to accomplish anything.',
           source: 'Nipsey Hussle',
+                 
   
   }
 ];
@@ -65,12 +69,19 @@ function printQuote() {
   var randomQuote = getRandomQuote(quotes);
                  yourStringHere += "<p class='quote'>" + randomQuote.quote + "</p>";
                                  yourStringHere += "<p class='source'>" + randomQuote.source;
+                                 if(randomQuote.citation){
                                                  yourStringHere += "<span class='citation'>" + randomQuote.citation + "</span>";
+                                 }
+                                 if(randomQuote.year) {
                                                                 yourStringHere += "<span class='year'>" + randomQuote.year + "</span>"
+                                 }
                                                                                 yourStringHere += "</p>";
 
   document.getElementById('quote-box').innerHTML = yourStringHere;
+
 }
+// Create conditional statements to determine what prints  to the page 
+// 
 
 printQuote();
 
